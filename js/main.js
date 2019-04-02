e59da087958f4f3f2579628e9ad8513661d2c181
yura <webcodeua@gmail.com>
2019-04-02 17:25:09 +0300

    commit 020419

--- js/main.js
+++ js/main.js
@@ -1,28 +1,28 @@
-$(function () {
+// $(function () {
 
 
-	$('.load-more').on('click', function () {
-		const btn = $(this);
-		const loader = btn.find('span');
-		$.ajax({
-			url: '/date.html',
-			type: 'GET',
-			beforeSend: function () {
-				btn.attr('disebled', true);
-				loader.addClass('d-inline-block');
+// 	$('.load-more').on('click', function () {
+// 		const btn = $(this);
+// 		const loader = btn.find('span');
+// 		$.ajax({
+// 			url: '/date.html',
+// 			type: 'GET',
+// 			beforeSend: function () {
+// 				btn.attr('disebled', true);
+// 				loader.addClass('d-inline-block');
 
-			},
-			succes: function () {
-				setTimeout(function () {
-					loader.removeClass('d-inline-block');
-				}, 1500);
-				btn.attr('disebled', false);
-				$('.after-post').before(response);
-			},
-			error: function () {
-				alert('ERROR');
-				btn.attr('disebled', false);
-			}
-		});
-	});
-});
+// 			},
+// 			succes: function () {
+// 				setTimeout(function () {
+// 					loader.removeClass('d-inline-block');
+// 				}, 1500);
+// 				btn.attr('disebled', false);
+// 				$('.after-post').before(response);
+// 			},
+// 			error: function () {
+// 				alert('ERROR');
+// 				btn.attr('disebled', false);
+// 			}
+// 		});
+// 	});
+// });
